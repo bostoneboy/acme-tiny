3f103d29c067118ef23bf33d5aef37903a257fff
Bill JaJa <bostoneboy@gmail.com>
2018-01-11 11:16:41 +0800

    Update acme_tiny.py
    small fix

--- acme_tiny.py
+++ acme_tiny.py
@@ -110,7 +110,7 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA):
         wellknown_path = os.path.join(acme_dir, token)
         with open(wellknown_path, "w") as wellknown_file:
             wellknown_file.write(keyauthorization)
-            os.system("cd /srv/blog.pagebrin/.deploy_git;git add .;git commit -m 'add acme verification file';git push -u git@github.com:bostoneboy/blog.git  --force")
+            os.system("cd /srv/blog.pagebrin/.deploy_git;git add well-known/acme-challenge/*;git commit -m 'add acme verification file';git push -u git@github.com:bostoneboy/blog.git --force")
 
         # check that the file is in place
         wellknown_url = "http://{0}/well-known/acme-challenge/{1}".format(domain, token)
